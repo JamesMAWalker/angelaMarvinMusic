df850ba6720ab812880320c08e96bb86fb206d97
James Walker <jamesmawalker@gmail.com>
2019-12-16 20:00:40 +0700

    Remove settings.js

--- settings.js
+++
@@ -1,4 +0,0 @@
-exports.themeLocation = "./wp-content/themes/angela-marvin-music/";
-exports.urlToPreview = "http://angela-marvin-music.local";
-
-// Change .urlToPreview to whatever the name of the local devserver is for the current project. 
