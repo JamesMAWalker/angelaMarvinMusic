709ac794979bdf53ccdcfe94b0978c2e96535709
James Walker <jamesmawalker@gmail.com>
2019-12-19 16:49:45 +0700

    Add section frames, and most of blog section.

--- home.php
+++ home.php
@@ -10,5 +10,120 @@ get_header();
             </div>
         </div>
     </section>
+    <secition class="section-listen">
+        <div class="listen">
+           // Custom listen post type goes here //
+        </div>
+    </secition>
+    <section class="section-blog">
+        <div class="blog-frame">
+            <div class="blog-wrapper">
+                <div class="blog">
+                    <div class="blog__spine"></div>
+                    <div class="blog__item blog__item-1">
+                        <div class="blog__item-details sub-header-2 sub-header-2">
+                            <span class="blog__item-category">PRACTICE |</span>
+                            <span class="blog__item-date">12.12.19</span>
+                        </div>
+                        <div class="blog__item-title sub-header-1-dark">
+                            Double-stops and YOU
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-2">
+                        <div class="blog__item-details sub-header-2">
+                            <span class="blog__item-category">PERFORMANCE |</span>
+                            <span class="blog__item-date">11.03.19</span>
+                        </div>
+                        <div class="blog__item-title">
+                            My First Concerto
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing...
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-3">
+                        <div class="blog__item-details sub-header-2">
+                            <span class="blog__item-category">Listening |</span>
+                            <span class="blog__item-date">11.01.19</span>
+                        </div>
+                        <div class="blog__item-title">
+                            Dreaming of Du Pre
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-4">
+                        <div class="blog__item-details sub-header-2">
+                            <span class="blog__item-category">THEORY |</span>
+                            <span class="blog__item-date">10.21.19</span>
+                        </div>
+                        <div class="blog__item-title">
+                            Piano for Cellists
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-5">
+                        <div class="blog__item-details sub-header-2">
+                            <span class="blog__item-category">PRACTICE |</span>
+                            <span class="blog__item-date">10.05.19</span>
+                        </div>
+                        <div class="blog__item-title ">
+                            I Learned BASS
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing...
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-6">
+                        <div class="blog__item-details sub-header-2">
+                            <span class="blog__item-category">PRACTICE |</span>
+                            <span class="blog__item-date">09.29.19</span>
+                        </div>
+                        <div class="blog__item-title">
+                            Dreaming Small
+                        </div>
+                        <div class="blog__item-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        </div>
+                        <a href="#" class="blog__item-link">
+                            <img src="<?php echo get_template_directory_uri()?>/assets/images/Read More - Button.svg" alt="" class="read-more-button">
+                        </a>
+                    </div>
+                    <div class="blog__item blog__item-7 text-button">see more &rarr;</div>
+                </div>
+            </div>
+            <div class="blog__title">
+                <span class="header-2">
+                    read
+                </span>
+            </div>
+        </div>
+    </section>
+    <section class="section-contact">
+        <div class="contact">
+            // Contact form goes here //
+        </div>
+    </section>
 </body>
 </html>
