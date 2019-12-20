20d11f7fc49a95c7ac9ac759ef9258ebf572036e
James Walker <jamesmawalker@gmail.com>
2019-12-20 17:16:50 +0700

    Complete template structuring.

--- home.php
+++ home.php
@@ -1,24 +1,12 @@
 <?php
+
 get_header();
+
+    get_template_part('template-parts/hero'); 
+    get_template_part('template-parts/music'); 
+    get_template_part('template-parts/blog');
+    get_template_part('template-parts/contact');
+
+get_footer();
+
 ?>
-
-    <section class="section-hero">
-        <div class="hero hero-bg-container">
-            <div class="hero__container">
-                <h1 class="hero__title header-1">Angela Marvin</h1>
-                <div class="hero__subtitle sub-header-1">cellist | musician | educator</div>
-            </div>
-        </div>
-    </section>
-    <?php
-        get_template_part('template-parts/music'); 
-        get_template_part('template-parts/blog');
-
-    ?>
-    <section class="section-contact">
-        <div class="contact">
-            // Contact form goes here //
-        </div>
-    </section>
-</body>
-</html>
