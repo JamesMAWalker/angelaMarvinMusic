9b2ae98c3848d2ec2d0414dba1011b49272b3b3d
Andrew Marvin <36093643+andrewtmarvin@users.noreply.github.com>
2019-12-20 13:08:19 +0700

    test

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
         <footer>
             <p>Template Footer | All Rights Reserved</p>
             <p>testing</p>
+            <p>testing 2</p>
         </footer>
         <?php wp_footer(); ?>
     <body>
