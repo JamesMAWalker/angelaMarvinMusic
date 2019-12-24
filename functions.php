62034891b9a1b1803362d3dc9c2105924d538ef0
James Walker <jamesmawalker@gmail.com>
2019-12-24 14:32:55 +0700

    Add scrollout lib.

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function angelamusic_files() {
     wp_enqueue_style('angelamusic_main_files', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
     wp_enqueue_script('angelamusic_js', get_template_directory_uri() . '/assets/js/index.js', '1.0.0', true);
+    wp_enqueue_script('scrollout_js', get_template_directory_uri() . '/assets/js/vendors/scrollout.min.js', '1.0.0', true);
     wp_enqueue_script('jquery');
 }
 
--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 	<nav class="nav <?php if ($post->ID === 28) {echo 'nav--about';} ?>" data-scroll>
 		<a href="#section-listen" class="nav__item " data-scroll>music</a>
 		<a href="#section-blog" class="nav__item " data-scroll>blog</a>
-		<img class="nav__logo" src="<?php echo get_template_directory_uri()?>/assets/images/provisional-logo.svg" alt="angela marvin monogram">
+		<img class="nav__logo" src="<?php echo get_template_directory_uri()?>/assets/images/provisional-logo-blu.svg" alt="angela marvin monogram">
 		<a href="<?php get_home_url()?>/about" class="nav__item " data-scroll>about</a>
 		<a href="#section-contact" class="nav__item " data-scroll>contact</a>
 	</nav>
