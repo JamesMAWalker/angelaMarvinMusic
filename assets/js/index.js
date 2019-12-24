1fc54a42544dcf9114922778ec17b8eefd4087a6
Andrew Marvin <36093643+andrewtmarvin@users.noreply.github.com>
2019-12-24 16:45:26 +0700

    blog section forward back functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,24 @@
+// jQuery(document).ready(function($) {
+//     $('.blog__item-7').on("click", function(){
+//         $(this).css('display', 'none');
+//         console.log($(this).prevAll('blog__item'));
+//         $(this).prevAll('blog__item').css('display', 'none');
+//     })
+// });
+
+
+
 jQuery(document).ready(function($) {
     $('.blog__item-7').on("click", function(){
-        alert('test');
-        $(this).nextAll('blog__item-7').slice(0,4).css('display', 'flex');
+        $(this).prevAll(".blog__item").css('display', 'none');
+        $(this).css('display', 'none');
+        $(this).nextAll(".blog__item").slice(0,8).css('display', 'flex');
+
     })
-  })
+    $('.blog__item-8').on("click", function(){
+        $(this).nextAll(".blog__item").css('display', 'none');
+        $(this).css('display', 'none');
+        $(this).prevAll(".blog__item").slice(0,8).css('display', 'flex');
+
+    })
+});
