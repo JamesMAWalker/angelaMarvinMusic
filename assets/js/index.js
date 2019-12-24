771454faf29ed1092b4344acb7047c7e3300117b
James Walker <jamesmawalker@gmail.com>
2019-12-24 18:30:15 +0700

    Tweak mobile menu.

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,4 +23,16 @@ jQuery(document).ready(function($) {
         $(this).prevAll(".blog__item").slice(0,8).removeClass('hide');
 
     })
+
+    //mobile nav 
+    $('.nav__btn-phone').on('click', () => {
+        $('.nav').toggleClass('unhide--flex-height');
+
+        if ($('.nav').hasClass('unhide--flex-height')) {
+            $('.nav').css('height', '60vh');
+            $('.nav').css('transition', 'all .2s');
+        } else {
+            $(".nav").css("height", "0");
+        }
+    })
 });
