d85690412c44af61c125364712955cf9b5bc3cad
James Walker <jamesmawalker@gmail.com>
2019-12-25 21:46:05 +0700

    Hero MQs.

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -2,7 +2,7 @@
         <div class="hero hero-bg-container">
             <div class="hero__container">
                 <h1 class="hero__title header-1">Angela Marvin</h1>
-                <div class="hero__subtitle sub-header-1">artist | performer | educator</div>
+                <div class="hero__subtitle sub-header-1">artist <span>|</span> performer <span>|</span> educator</div>
             </div>
         </div>
     </section>
