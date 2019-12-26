792b0eabbf91f04e23e91b3a5f53afab898a9506
James Walker <jamesmawalker@gmail.com>
2019-12-26 19:49:46 +0700

    Add MQs for about page, tweak contact.

--- single-post.php
+++ single-post.php
@@ -20,15 +20,9 @@ wp_head();
             <?php 
             the_content();
             ?>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque iste, autem pariatur quibusdam minus, vel tempore, commodi praesentium et id omnis alias sapiente. Maxime, ex quia nostrum rerum soluta ipsam.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quae animi beatae alias ullam, nisi aliquid eligendi ipsam obcaecati repellat perferendis iusto molestiae voluptatum magni quasi blanditiis exercitationem quia eum!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo earum recusandae illum officiis culpa hic soluta libero. Esse saepe fugit, optio itaque perferendis, natus quaerat corrupti ducimus qui magnam amet!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis officia maiores in! Quam quia dicta deserunt minus id amet maxime possimus debitis, sit provident, aliquid quo officiis vel eaque ratione!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi fugiat officia molestiae laboriosam aliquid aperiam sapiente veniam tempore, porro nihil laborum amet saepe ratione perferendis. Reprehenderit cumque repellat molestias similique.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus cum temporibus reprehenderit odio distinctio eos culpa tempora, ipsa, perspiciatis voluptatem iusto libero in sint nesciunt natus accusamus qui vero quo?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic similique debitis facere tempora, nihil magni voluptatibus, odit velit optio placeat impedit, vel architecto. Quis facilis illo, minima ducimus voluptatum sapiente.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ut illum ullam possimus quia nemo culpa voluptatum repellendus vel quisquam! Nostrum ea adipisci dignissimos similique! Eligendi odio facilis natus earum?
-            <img class="single-blog__btn btn__home" src="<?php echo get_template_directory_uri()?>/assets/images/back-home-btn.svg" alt="" class="read-more-button">
+            <a href="/">
+                <img class="single-blog__btn btn__home" src="<?php echo get_template_directory_uri()?>/assets/images/back-home-btn.svg" alt="" class="read-more-button">
+            </a>
         </div>
     </div>      
 </div>
