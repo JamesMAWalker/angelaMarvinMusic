cb46930b689eaadd5c5a6b9102f6087c59b20b20
Andrew Marvin <36093643+andrewtmarvin@users.noreply.github.com>
2020-01-04 19:20:42 +0700

    implemented music section JS to link buttons to media

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function angelamusic_files() {
     wp_enqueue_style('angelamusic_main_files', get_template_directory_uri() . '/style.css', array(), '1.0.1', 'all');
     wp_enqueue_script('angelamusic_js', get_template_directory_uri() . '/assets/js/index.js', '1.0.1', true);
     wp_enqueue_script('scrollout_js', get_template_directory_uri() . '/assets/js/vendors/scrollout.min.js', '1.0.0', true);
+    // wp_enqueue_script('YouTube_api', "https://www.youtube.com/iframe_api", '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'angelamusic_files');
@@ -15,7 +16,6 @@ function my_function_admin_bar() { return false; }
 add_filter( 'show_admin_bar' , 'my_function_admin_bar');
 
 function eq_add_favicon(){ ?>
-    <!-- Custom Favicons -->
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri();?>/assets/images/favicon.png"/>
     <?php 
 }
